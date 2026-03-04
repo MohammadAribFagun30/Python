889bc7980b93a796181a69c6c23d2850874a3739
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-03-05 00:06:45 +0600

    New

---
+++ Clever_Conditional.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age : "))
+vote = ("Yes,can","No,cannot") [age<18] 
+#Ekne if condition true hole "No,cannot" hbe ..else "Yes,can" eta hbe
+print(vote)
+print() # Create new line
+print()
+#Another :
+income =  int(input("Enter your salary : "))
+
+tax = income*(0.5, 0.2) [income<200]
+print(tax)
---
+++ Unique_Conditional.py
@@ -0,0 +1,4 @@
+Age = int(input("Enter age : "))
+age = "Yes, can vote" if(Age>=18) else "No, Cannot vote"
+print(age)
+
