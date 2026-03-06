db1b3b80a72f736e456b742f21b5ea06a195aa5a
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-03-06 16:59:53 +0600

    Updated

--- User_Input.py
+++ User_Input.py
@@ -1,5 +1,8 @@
 name = input("My name is : ")
 age = int(input("My age is : "))
+gpa = float(input("2nd semester :"))
 print(name)
 print(age)
+print(gpa)
+
 print("Alhamdulillah I got CGPA-4 in my 2nd semester.")
