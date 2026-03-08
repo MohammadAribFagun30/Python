9a3081cc99ffdb6104782e2cef4b673acaf6bdff
MohammadAribFagun30 <mohammadarib23@gmail.com>
2026-03-09 02:00:34 +0600

    New

---
+++ For.py
@@ -0,0 +1,4 @@
+num= [1,2,3,4,5]
+# printing value with for loop
+for value in num :
+    print(value)
--- While.py
+++ While.py
@@ -4,9 +4,36 @@ while count<=5 :
  print("Hello ! ")
  count +=1
 
- 
    # Hello ! 
    # Hello ! 
    # Hello ! 
    # Hello ! 
    # Hello !
+
+   # problem-1 :
+   # printiing numbers :
+num = [1,2,3,4,5]
+
+index = 0
+
+while index < len(num) :
+    print(index)
+    index+=1
+
+    # problem-2 :
+    # search a number :
+    nums = [1,2,3,4,5,6]
+
+    x = 4
+    i = 0
+
+nums = [1,2,3,4,5,6]
+
+x = 4
+i = 0
+
+while i < len(nums):
+    if nums[i] == x:
+        print("Found it in position:", i)
+        break
+    i += 1
